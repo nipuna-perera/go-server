b2b74d8bf53d7660d80534343dd2781d9d62f12d
Nipuna Perera <nipuna.royal@gmail.com>
2021-03-28 10:46:08 -0500

    adding basic file serving

--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
-func main() {
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	uril := fmt.Sprintf("Request URI: %v", r.RequestURI)
+	log.Println(uril)
+	fmt.Fprintf(w, uril)
+}
 
+
+func serveFile(w http.ResponseWriter, r *http.Request){
+	ldir, _:=os.Getwd()
+
+	http.ServeFile(w, r, ldir+r.URL.Path)
+}
+func main() {
+	http.HandleFunc("/hello", sayHello)
+	http.HandleFunc("/", serveFile)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
